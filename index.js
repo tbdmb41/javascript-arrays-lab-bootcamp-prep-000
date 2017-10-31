6d2adebb04cd3b570e7ad393497a2996f74112cf
Thomas Bowen <tbowen1@outlook.com>
2017-10-31 19:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = "I don't do much."
 
 var cats = ["Milo", "Otis", "Garfield"]
-
-cats.slice(1)
-
-destructivelyAppendKitten("Otis"); 
+cats.slice(1) //["Garfield", Otis"]
+cats // ["Milo", "Garfield", "Otis"]
